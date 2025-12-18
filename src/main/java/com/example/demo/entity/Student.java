920178dbc75e9cb1fb7a628b0968c203be477088
bala-amypo <bala@amypo.in>
2025-12-18 09:30:40 +0000

    User commit

--- src/main/java/com/example/demo/entity/Student.java
+++ src/main/java/com/example/demo/entity/Student.java
@@ -4,7 +4,7 @@ import java.time.LocalDate;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
-import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 
@@ -13,7 +13,7 @@ public class Student {
     @Id
     @GeneratedValue(strategy=GenerationType.IDENTITY)
      private Long id;
-     @NotEmpty(message="Name should not be null")
+     @NotBlank(message="Name should not be null")
      private String name;
      private String dept;
      private LocalDate dob;
