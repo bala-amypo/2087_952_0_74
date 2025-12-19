ea5ae462d4aaf40222904f5692f05bb4dcb7b8f9
bala-amypo <bala@amypo.in>
2025-12-19 04:23:37 +0000

    User commit

--- src/main/java/com/example/demo/exception/PasswordNotMatchCri.java
+++ src/main/java/com/example/demo/exception/PasswordNotMatchCri.java
@@ -1,3 +1,11 @@
 package com.example.demo.exception;
 
-public class PatternNotMatchCri extends RuntimeException
+public class PatternNotMatchCri extends RuntimeException{
+    public PasswordNotMatchCri(String msg){
+        super(msg);
+    }
+}
+
+if(pass.length()<8){
+    throw new PasswordNotMatchCri("Password should have 8 char");
+}
